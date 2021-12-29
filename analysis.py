03380dd6176ea98760a1b1de2c719f25767e1c4d
nannullna <nannullna00@gmail.com>
2021-12-30 00:37:37 +0900

    feat: get available pushes

--- analysis.py
+++ analysis.py
@@ -1,9 +1,11 @@
 import os
+import json
 import argparse
 import datetime
 from datetime import timedelta
 from typing import Tuple, Optional
 
+import numpy as np
 import pandas as pd
 
 from data_update.prepare_data import prepare_login_df, prepare_push_df
@@ -19,10 +21,59 @@ def prepare_data(args) -> Tuple[pd.DataFrame]:
 
     return login_df, push_df
 
+def get_available_pushes(
+    push_df: pd.DataFrame, 
+    window_before_in_hours: int=48, 
+    window_after_in_hours: int=24, 
+    interval_in_hours: int=168,
+    sampling_type: str="before",
+):
+
+    available_types = ["before", "after", "both"]
+    if sampling_type not in available_types:
+        raise ValueError(f"type not in available types {available_types}")
+
+    available_pushes = []
+
+    game_ids = push_df["game_id"].unique()
+    print(f"Total of {len(game_ids)} available games")
+
+    for game_id in game_ids:
+        subset = push_df[push_df["game_id"] == game_id]
+
+        for push_id in range(len(subset)):
+            
+            time_1 = subset["pushTime"].iloc[push_id]
+            start_1 = time_1 - timedelta(hours=window_before_in_hours)
+            end_1 = time_1 + timedelta(hours=window_after_in_hours)
+            
+            count_1 = np.sum((subset["pushTime"] >= start_1) & (subset["pushTime"] <= end_1))
+
+            time_0 = time_1 - timedelta(hours=interval_in_hours)
+            start_0 = time_0 - timedelta(hours=window_before_in_hours)
+            end_0 = time_0 + timedelta(hours=window_after_in_hours)
+
+            count_0 = np.sum((subset["pushTime"] >= start_0) & (subset["pushTime"] <= end_0))
+
+            if (count_0 == 0) and (count_1 >= 1):
+                result = {k: str(push_df.iloc[push_id, i]) for i, k in enumerate(push_df.iloc[push_id].index)}
+                result.update({"count_1": int(count_1)})
+                result.update({"count_0": int(count_0)})
+                available_pushes.append(result)
+            
+    return available_pushes
+
+
 def main(args):
     login_df, push_df = prepare_data(args)
     print(f"total length of login df: {len(login_df)}, total length length of push df: {len(push_df)}")
     print()
+
+    available_pushes = get_available_pushes(push_df, args.window_before, args.window_after, args.interval, sampling_type="before")
+    print(f"Number of available pushes: {len(available_pushes)}")
+
+    with open("available_pushes.json", "w") as f:
+        json.dump(available_pushes, f, indent=4, ensure_ascii=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Do analysis on push and log data')
@@ -42,6 +93,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--window_before', type=int, metavar='HOURS', default=48, help='window size before the push time in hour. Look for the game_id/time available to sample (default: 48)')
     parser.add_argument('--window_after', type=int, metavar='HOURS', default=24, help='window size after the push time in hour. Look for the game_id/time available to sample (default: 24)')
+    parser.add_argument('--interval', type=int, metavar='HOURS', default=168, help='interval between t=0 and t=1. Look for the game_id/time available to sample (default: 168)')
 
     args = parser.parse_args()
 
